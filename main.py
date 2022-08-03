9e4272ff08e393b8be5b6a6fa740607ef9c1b309
Bryce Fisher <101945315+bryceefisher@users.noreply.github.com>
2022-08-02 18:22:00 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import smtplib
+import datetime as dt
+import random
+
+# ------------------------Constants---------------------------------------------------#
+
+MY_EMAIL = "sidestewbrew@gmail.com"
+PASSWORD = "ofxlfpugcpuicedu"
+DATE = dt.datetime.now()
+DAY = DATE.day
+RANDOM_NUM = random.randint(1, 102)
+
+if DAY == 2:
+    with open("quotes.txt") as quotes:
+        quote_list_rough = list(quotes.readlines())
+        quote_list = []
+        for quote in quote_list_rough:
+            quote_list.append(quote.replace("\n", ""))
+
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=PASSWORD)
+        connection.sendmail(from_addr=MY_EMAIL,
+                            to_addrs="sidestewbrew@yahoo.com",
+                            msg=f"Subject:Motivational Monday\n\n{quote_list[RANDOM_NUM]}")
